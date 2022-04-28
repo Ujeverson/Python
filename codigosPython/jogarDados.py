c7ad390f563f414dbdf165a734658517b1da8409
ujeverson <ujeverson@gmail.com>
2022-04-28 11:22:43 -0300

    Separando o código em funções

--- codigosPython/jogarDados.py
+++ codigosPython/jogarDados.py
@@ -1,6 +1,11 @@
 import random
 
 continua = True
+def gerarfaces(facesDado):
+    dados = int(input("Com quantos dados quer jogar?")) #entrada da quantidade de dados que o usuário quer jogar.
+    for i in range(dados):
+        numero = random.randint(1, facesDado)
+        print("\n Você jogou o dado",i+1, "e a face de cima é o:", numero)
 
 while continua:
     facesDado = input("\nDigite quantas faces tem o seu dado ou digite 'parar' para guardar os dados:")
@@ -10,9 +15,7 @@ while continua:
     
     else:
         facesDado = int(facesDado)
-        dados = int(input("Com quantos dados quer jogar?")) #entrada da quantidade de dados que o usuário quer jogar.
-        for i in range(dados):
-            numero = random.randint(1, facesDado)
-            print("\n Você jogou o dado",i+1, "e a face de cima é o:", numero)
+        gerarfaces(facesDado)
+       
 
 print("Agora você guardou os dados!")
