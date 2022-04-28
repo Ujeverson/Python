abb7c500001ee4c29e3c4359734e9c2fb4cc6ce8
ujeverson <ujeverson@gmail.com>
2022-04-28 11:20:54 -0300

    Colocando funções

--- codigosPython/jogarDados.py
+++ codigosPython/jogarDados.py
@@ -3,16 +3,16 @@ import random
 continua = True
 
 while continua:
-    facesDado = input("Digite quantas faces tem o seu dado ou digite 'parar' para guardar os dados:")
+    facesDado = input("\nDigite quantas faces tem o seu dado ou digite 'parar' para guardar os dados:")
     
     if facesDado == "parar":
-        continue = False
+        continua = False
     
     else:
         facesDado = int(facesDado)
         dados = int(input("Com quantos dados quer jogar?")) #entrada da quantidade de dados que o usuário quer jogar.
         for i in range(dados):
             numero = random.randint(1, facesDado)
-            print("Você jogou o dado e a face de cima é o:", numero)
+            print("\n Você jogou o dado",i+1, "e a face de cima é o:", numero)
 
 print("Agora você guardou os dados!")
